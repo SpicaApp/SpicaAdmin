4efb8139386b4f7c31a8c56002bdfd7df460c6fb
adrianbaumgart <adrianbaumgart@users.noreply.github.com>
2020-08-06 14:32:03 +0000

    deploy: 1343270a6a7722c7dd61365091bd69e7d04d12d7

---
+++ precache-manifest.afe777878165874f2e28bfed75be50cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36e12d24361488b62777037f76959f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc03ff77111e10a9577e",
+    "url": "/static/css/main.d483a227.chunk.css"
+  },
+  {
+    "revision": "36517f3900a9ee6874ea",
+    "url": "/static/js/2.ed9d8500.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/static/js/2.ed9d8500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc03ff77111e10a9577e",
+    "url": "/static/js/main.79a54fe0.chunk.js"
+  },
+  {
+    "revision": "86a49bbf46162663a16d",
+    "url": "/static/js/runtime-main.c1dc4482.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SpicaAdmin/precache-manifest.746f737d254cea88005fa72107821dd7.js"
+  "/precache-manifest.afe777878165874f2e28bfed75be50cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpicaAdmin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
